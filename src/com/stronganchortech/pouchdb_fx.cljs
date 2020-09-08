4279a4a9cd3170ca73df22b114f011091c92a9a7
Aaron Decker <decker49@gmail.com>
2020-09-08 16:54:48 -0500

    Initial commit.

---
+++ src/com/stronganchortech/pouchdb_fx.cljs
@@ -0,0 +1,10 @@
+(ns com.stronganchortech.pouchdb-fx
+  (:require
+   [re-frame.core :as rf]
+   ["pouchdb" :as pouchdb]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!")
+  x)
